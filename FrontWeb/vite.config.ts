cbf55f715a6e869308e47a3a8b8f0aef047b2476
Sloyi <rayan.es-seffar@epitech.eu>
2023-10-09 15:10:12 +0200

    feat: ssl certificat for front

--- FrontWeb/vite.config.ts
+++ FrontWeb/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 //host: true
+
+const options = {
+  key: fs.readFileSync('./privkey.pem'),
+  cert: fs.readFileSync('./fullchain.pem'),
+};
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    https: options,
   }
 })
