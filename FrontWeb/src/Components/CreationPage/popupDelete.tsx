9441fdc9a7d3f76b1190c506c9ff8e02d5fbe575
Taha ALANI <taha.alani@epitech.eu>
2023-11-04 23:06:56 +0100

    fix(frontweb): fix button

--- FrontWeb/src/Components/CreationPage/popupDelete.tsx
+++ FrontWeb/src/Components/CreationPage/popupDelete.tsx
@@ -43,7 +43,7 @@ function DeleteCard({ areaMap }: { areaMap: Area }) {
       <button
         data-modal-target="popup-modal"
         onClick={toggleModal}
-        className="rounded-full block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="rounded-full block text-white bg-secondary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center dark:bg-secondary dark:hover:bg-secondary dark:focus:ring-blue-800"
         type="button"
       >
         {t("delete")}
