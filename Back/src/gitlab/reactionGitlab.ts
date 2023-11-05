68a3d31c22e05e71298111140a9742cafa54a86c
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-11-05 16:24:00 +0100

    feat: add reaction gitlab and github

--- Back/src/gitlab/reactionGitlab.ts
+++ Back/src/gitlab/reactionGitlab.ts
@@ -47,4 +47,94 @@ export class ReactionGitlab {
         console.log(error.response.data);
       });
   }
+
+  async createBranch(userService: UserServiceEntity, arg: any) {
+    const headers = {
+      Authorization: `Bearer ${userService.token}`,
+    };
+    await axios
+      .post(
+        'https://gitlab.com/api/v4/projects/' +
+          arg.project_id +
+          '/repository/branches?branch=' +
+          arg.branch_name +
+          '&ref=' +
+          arg.ref,
+        {},
+        { headers: headers },
+      )
+      .then((response) => {
+        console.log("Branch created");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }
+
+  async deleteBranch(userService: UserServiceEntity, arg: any) {
+    const headers = {
+      Authorization: `Bearer ${userService.token}`,
+    };
+    await axios
+      .delete(
+        'https://gitlab.com/api/v4/projects/' +
+          arg.project_id +
+          '/repository/branches/' +
+          arg.branch_name,
+        { headers: headers },
+      )
+      .then((response) => {
+        console.log("Branch deleted");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }
+
+  async createMergeRequest(userService: UserServiceEntity, arg: any) {
+    const headers = {
+      Authorization: `Bearer ${userService.token}`,
+    };
+    await axios
+      .post(
+        'https://gitlab.com/api/v4/projects/' +
+          arg.project_id +
+          '/merge_requests?source_branch=' +
+          arg.source_branch +
+          '&target_branch=' +
+          arg.target_branch +
+          '&title=' +
+          arg.title +
+          '&description=' +
+          arg.description,
+        {},
+        { headers: headers },
+      )
+      .then((response) => {
+        console.log("Merge request created");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }
+
+  async deleteMergeRequest(userService: UserServiceEntity, arg: any) {
+    const headers = {
+      Authorization: `Bearer ${userService.token}`,
+    };
+    await axios
+      .delete(
+        'https://gitlab.com/api/v4/projects/' +
+          arg.project_id +
+          '/merge_requests/' +
+          arg.merge_request_id,
+        { headers: headers },
+      )
+      .then((response) => {
+        console.log("Merge request deleted");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }
 }
