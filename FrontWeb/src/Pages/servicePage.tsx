33ed27aee328c43c1e65f1ce5c4da95d810ea263
Taha ALANI <taha.alani@epitech.eu>
2023-11-05 19:17:16 +0100

    fix(frontweb): fix account button

--- FrontWeb/src/Pages/servicePage.tsx
+++ FrontWeb/src/Pages/servicePage.tsx
@@ -58,20 +58,14 @@ export default function Service() {
     setPopupDataReaction(null);
   };
 
-  const AccountConnection = async () => {
-    try {
-      await axios.get(
-        import.meta.env.VITE_DNS_NAME +
-          ":8080/api/auth/" +
-          selectedService.bottomText +
-          "?token=" +
-          localStorage.getItem("token")
-      );
-  
-      navigate("/servicePage");
-    } catch (error) {
-      console.error("Erreur lors de la requête :", error);
-    }
+  const AccountConnection = () => {
+    window.location.href =
+      import.meta.env.VITE_DNS_NAME +
+      ":8080/api/auth/" +
+      selectedService.bottomText +
+      "?token=" +
+      localStorage.getItem("token");
+    navigate("/servicePage");
   };
 
   const getServices = () => {
